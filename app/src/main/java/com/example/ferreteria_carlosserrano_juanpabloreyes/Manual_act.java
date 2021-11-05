49b2e1c8a41b353bb50690cb22ec538cc8a32f11
Gamer <carlosfelipe.sc.16@gmail.com>
2021-11-05 02:14:52 -0300

    puse los textos en setVisibility(View.INVISIBLE); luego puse los textos en setVisibility(View.VISIBLE);

--- app/src/main/java/com/example/ferreteria_carlosserrano_juanpabloreyes/Manual_act.java
+++ app/src/main/java/com/example/ferreteria_carlosserrano_juanpabloreyes/Manual_act.java
@@ -26,6 +26,9 @@ public class Manual_act extends AppCompatActivity {
         txtma = findViewById(R.id.txtma);
         v = findViewById(R.id.vw);
 
+        txt.setVisibility(View.INVISIBLE);
+        txtma.setVisibility(View.INVISIBLE);
+
 
 
         Bundle bun = getIntent().getExtras();
@@ -44,31 +47,39 @@ public class Manual_act extends AppCompatActivity {
         if (manu.getSelectedItem().toString().equals("Pintar mesa")) {
             txt.setText("Herramientas: " + fe.getHerramienta()[1] + ", " + fe.getHerramienta()[9]);
             txtma.setText(" Material: " + fe.getMaterial()[1] + ", " + fe.getMaterial()[2] + ", " + fe.getMaterial()[3] + ", " + fe.getMaterial()[6] + ", " + fe.getMaterial()[13]);
-
+            txt.setVisibility(View.VISIBLE);
+            txtma.setVisibility(View.VISIBLE);
         }
         if (manu.getSelectedItem().toString().equals("Instalar ventilador de techo")) {
-            txt.setText("Herramientas: " + fe.getHerramienta()[4] + ", " + fe.getMaterial()[7]);
-            txtma.setText(" Material: " + fe.getMaterial()[12] + ", " + fe.getMaterial()[14] + ", " + fe.getMaterial()[15] + ", " + fe.getMaterial()[22]);
+            txt.setText("Herramientas: " + fe.getHerramienta()[4] + ", " + fe.getHerramienta()[7]);
+            txtma.setText(" Material: " + fe.getMaterial()[12] + ", " + fe.getMaterial()[14] + ", " + fe.getMaterial()[15] + ", " + fe.getMaterial()[15]);
+            txt.setVisibility(View.VISIBLE);
+            txtma.setVisibility(View.VISIBLE);
+        }
+        if (manu.getSelectedItem().toString().equals("Arreglar puertas de la cocina")) {
+            txt.setText("Herramientas: " + fe.getHerramienta()[7] + ", " + fe.getHerramienta()[16] + ", " + fe.getHerramienta()[11]);
+            txtma.setText(" Material: " + fe.getMaterial()[17]+ ","+ fe.getMaterial()[17]+","+ fe.getMaterial()[13]);
+            txt.setVisibility(View.VISIBLE);
+            txtma.setVisibility(View.VISIBLE);
         }
         if (manu.getSelectedItem().toString().equals("Instalación de luz")) {
             txt.setText("Herramientas: " + fe.getHerramienta()[6]);
-            txtma.setText(" Material: " + fe.getMaterial()[12] + ", " + fe.getMaterial()[14] + ", " + fe.getMaterial()[15] + ", " + fe.getMaterial()[19] + " y " + fe.getMaterial()[22]);
+            txtma.setText(" Material: " + fe.getMaterial()[12] + ", " + fe.getMaterial()[14] + ", " + fe.getMaterial()[15] + ", " + fe.getMaterial()[19] + " y " + fe.getMaterial()[20]);
+            txt.setVisibility(View.VISIBLE);
+            txtma.setVisibility(View.VISIBLE);
         }
-        if (manu.getSelectedItem().toString().equals("Arreglar regadera")) {
-            txt.setText("Herramientas: " + fe.getHerramienta()[0] + ", " + fe.getMaterial()[6] + ", " + fe.getHerramienta()[14]);
-            txtma.setText(" Material: No se necesitara ningun material por el momento");
 
-        }
         //Preguntar
-        if (manu.getSelectedItem().toString().equals("Armar closet")) {
+        if (manu.getSelectedItem().toString().equals("Armar armario en forma L")) {
             txt.setText("Herramientas: " + fe.getHerramienta()[8] + ", " + fe.getHerramienta()[4] + ", " + fe.getHerramienta()[2]);
-            txtma.setText(" Material: " + fe.getMaterial()[15] + fe.getMaterial()[24] + ", " + fe.getMaterial()[25] + ", " + fe.getMaterial()[26] + ", " + fe.getMaterial()[27] + ", " + fe.getMaterial()[28] + ", " + fe.getMaterial()[29] + ", " + fe.getMaterial()[30] + ", " + fe.getMaterial()[31] + ", " + fe.getMaterial()[32] + " y " + fe.getMaterial()[33]);
+            txtma.setText(" Material: " + fe.getMaterial()[15] + fe.getMaterial()[20] + ", " + fe.getMaterial()[20] + ", " + fe.getMaterial()[20] + ", " + fe.getMaterial()[20] + ", " + fe.getMaterial()[20] + ", " + fe.getMaterial()[20] + ", " + fe.getMaterial()[20] + ", " + fe.getMaterial()[20] + ", " + fe.getMaterial()[20] + " y " + fe.getMaterial()[20]);
         }
 
         if (manu.getSelectedItem().toString().equals("Armar repisa")) {
             txt.setText("Herramientas: " + fe.getHerramienta()[0] + ", " + fe.getHerramienta()[6] + ", " + fe.getHerramienta()[14]);
-            txtma.setText(" Material: " + fe.getMaterial()[6] + ", " + fe.getMaterial()[25] + ", " + fe.getMaterial()[26] + ", " + fe.getMaterial()[15] + ", " + fe.getMaterial()[27] + ", " + fe.getMaterial()[28] + ", " + fe.getMaterial()[29] + ", " + fe.getMaterial()[30] + ", " + fe.getMaterial()[31] + ", " + fe.getMaterial()[32] + " y " + fe.getMaterial()[33]);
-
+            txtma.setText(" Material: " + fe.getMaterial()[12] + ", " + fe.getMaterial()[12]);
+            txt.setVisibility(View.VISIBLE);
+            txtma.setVisibility(View.VISIBLE);
         }
 
     }
@@ -91,6 +102,10 @@ public class Manual_act extends AppCompatActivity {
 
         }
 
+    }
+    public void Volver(View view){
+        Intent i = new Intent(this,Home_act.class);
+        startActivity(i);
     }
 
 }
