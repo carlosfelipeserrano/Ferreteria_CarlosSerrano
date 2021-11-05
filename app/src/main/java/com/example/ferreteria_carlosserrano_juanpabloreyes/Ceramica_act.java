7b8f07aff6c139d120302f02472b779bb07ad769
Gamer <carlosfelipe.sc.16@gmail.com>
2021-11-05 02:20:05 -0300

    nueva clase llamada Ceremica_act

---
+++ app/src/main/java/com/example/ferreteria_carlosserrano_juanpabloreyes/Ceramica_act.java
@@ -0,0 +1,14 @@
+package com.example.ferreteria_carlosserrano_juanpabloreyes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ceramica_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ceramica);
+    }
+}
