ea8d132a2c3c4cefc76b8fb1b14517abcea65284
Gamer <carlosfelipe.sc.16@gmail.com>
2021-11-05 02:12:56 -0300

    Cambie los precios en herramientas y material

--- app/src/main/java/Objetos/Ferreteria.java
+++ app/src/main/java/Objetos/Ferreteria.java
@@ -3,18 +3,18 @@ package Objetos;
 public class Ferreteria {
     private int id;
     private String[] material = {"Cemento","Lija","Pintura en Spray","Barniz", "Puertas", "Listones de madera", "Terciado", "Plancha de zinc", "Plancha de internit", "Guantes de fibra", "Cables electrico","Bolsa de clavos",
-            "Caja de pernos","Caja de tornillos","Casco de seguridad","Carretilla de acero","Bolsa de tarugos","Botella de diluyente","Interructores triple"};
+            "Caja de pernos","Caja de tornillos","Casco de seguridad","Carretilla de acero","Bolsa de tarugos","Botella de diluyente","Interructores triple","Caja de bisagras","Tablero melaminado"};
 
-    private String[] herramienta ={"Alicate", "Serrucho", "Brocha", "Huincha de medir","Llave Francesa","Multimetro digital","Desortillador","Taladro electrico","Escuadra",
+    private String[] herramienta ={"Alicate", "Serrucho", "Brocha", "Huincha de medir","Llave Francesa","Multimetro digital","Desortillador manual","Taladro electrico","Escuadra",
                                    "Martillo de fibra","Sierra Manual","Sierra electrica","Sierra Caladora","Pala de punta",
-                                   "Kits de niveles","Kit de instalación de ceramica","Kits de soldadora"};
+                                   "Kits de niveles","Kit de instalación de ceramica","Broca para madera","Kits de soldadora"};
 
-    private String[] manual ={"Pintar mesa", "Instalar ventilador de techo", "Arreglar regadera", "Instalación de luz", "Armar closet", "Armar repisa"," Armar despensa","Colocar Ceramica",
+    private String[] manual ={"Pintar mesa", "Instalar ventilador de techo", "Arreglar puertas de la cocina", "Instalación de luz", "Armar armario en forma L", "Armar repisa"," Armar despensa","Colocar Ceramica",
                               "Instalar llave de agua","Techo","Encialado de madera","Encialado de cemento","Cañaria del agua", "Estanque de baño ",
                                "Ducha electrica", "Lava manos"};
 
-    private int[] preciosma = {8000, 6500, 5000, 10000, 30000, 25000, 1500, 15000, 7000, 17000, 7000, 12000, 17000, 13000, 3000, 27000, 23000, 37000};
-    private int[] preciosher ={5000, 8000, 4000, 2500, 9000, 16000, 12000, 40000, 3100, 15000, 6500, 100000, 50000, 10000, 4100, 130000,150000};
+    private int[] preciosma = {8000, 6500, 5000, 10000, 30000, 25000, 22000, 15000, 27000, 7500, 7000, 12000, 17000, 14000, 3000, 27000, 12000, 7000, 16000,4000,4000, 8500, 24000};
+    private int[] preciosher ={5000, 8000, 4000, 2500, 9000, 16000, 12000, 40000, 3100, 15000, 6500, 100000, 50000, 10000, 4100, 130000,18000,150000};
 
     private int stock;
 
