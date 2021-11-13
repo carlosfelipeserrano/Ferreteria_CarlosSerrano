9d3b63add805c9f0fddc90c2081bd9cd209d3eee
Gamer <carlosfelipe.sc.16@gmail.com>
2021-11-13 16:28:09 -0300

    Agregue el video y las intrucciones

--- app/src/main/java/com/example/ferreteria_carlosserrano_juanpabloreyes/Ceramica_act.java
+++ app/src/main/java/com/example/ferreteria_carlosserrano_juanpabloreyes/Ceramica_act.java
@@ -2,13 +2,22 @@ package com.example.ferreteria_carlosserrano_juanpabloreyes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class Ceramica_act extends AppCompatActivity {
-
+private VideoView ce;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ceramica);
+        ce = findViewById(R.id.ce);
+
+        ce.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.cera));
+        MediaController mediaController = new MediaController(this);
+        ce.setMediaController(mediaController);
+        ce.start();
     }
 }
